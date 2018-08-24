937ed58a46d794c8ea7ea04dc6057d75d2a6a160
vanessa.vishakha <vanessa.vishakha@ithands.biz>
2018-08-24 12:35:39 +0530

    Add options page for footer

--- wp-content/themes/greenpeak/functions.php
+++ wp-content/themes/greenpeak/functions.php
@@ -156,3 +156,23 @@ add_filter( 'nav_menu_link_attributes', function ( $atts, $item, $args ) {
 }, 10, 3 );
 
 
+/*
+ * Option Page
+ */
+if( function_exists('acf_add_options_page') ) {
+
+    acf_add_options_page(array(
+        'page_title' 	=> 'Theme General Settings',
+        'menu_title'	=> 'Theme Settings',
+        'menu_slug' 	=> 'theme-general-settings',
+        'capability'	=> 'edit_posts',
+        'redirect'		=> false
+    ));
+
+    acf_add_options_sub_page(array(
+        'page_title' 	=> 'Theme Footer Settings',
+        'menu_title'	=> 'Footer',
+        'parent_slug'	=> 'theme-general-settings',
+    ));
+
+}
