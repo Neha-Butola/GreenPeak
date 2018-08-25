8afd3079c8bd80275cf9a9d0ed033029b9cbcfc5
Vikash <vikash@admins-MBP.lan>
2018-08-25 14:31:36 +0530

    media page css update

--- wp-content/themes/greenpeak/template/media.php
+++ wp-content/themes/greenpeak/template/media.php
@@ -4,6 +4,135 @@
 get_header(); ?>
 
 
+  <div class="inner-banner" style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/images/ContactBridge.jpg');">
+        <div class="heading-block">
+            <h1 class="small">MEDIA & RESOURCES</h1>
+        </div>
 
+        <div class="line-div-ani">
+        </div>
+
+    </div>
+
+
+    <!-- Media logo section starts here -->
+
+        <section class="media-logo-wrapper">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <p>Green Peak’s work and insights have been featured in the Harvard Business Review, 
+                            The Wall Street Journal, Investor’s Business Daily, Private Equity International and more. </p>
+                    </div>
+                    <div class="col-12 col-sm-4 logo-block">
+                        <figure>
+                            <img src="<?php echo get_template_directory_uri(); ?>/images/will-street.png" alt="" class="img-fluid">
+                        </figure>
+                    </div> 
+                    <div class="col-12 col-sm-4 logo-block">
+                        <figure>
+                            <img src="<?php echo get_template_directory_uri(); ?>/images/harverd-business.png" alt="" class="img-fluid">
+                        </figure>
+                    </div>
+                    <div class="col-12 col-sm-4 logo-block">
+                        <figure>
+                            <img src="<?php echo get_template_directory_uri(); ?>/images/bloomberg-logo.png" alt="" class="img-fluid">
+                        </figure>
+                    </div>
+                    <div class="col-12">
+                        <p>Take a deeper dive into the videos and articles below to learn how we are eradicating average 
+                            for our clients.   </p>
+                    </div>
+                </div>  
+                
+            </div>
+        </section>
+    <!-- Media logo section ends here -->
+
+    <!-- Video section starts here  -->
+        <section class="video-warpper">
+            <div class="container">
+                <h2>Video</h2>
+                <div class="row">
+                   
+                    <div class="col-12 video-block">
+                        <video width="100%" height="500px" controls>
+                            <source src="<?php echo get_template_directory_uri(); ?>/assets/videos/Green-Peak-Summer-Retreat-2018.mp4" type="video/mp4">
+                        </video>
+                    </div>
+                    <div class="col-12 video-block">
+                        <video width="100%" height="500px" controls>
+                            <source src="<?php echo get_template_directory_uri(); ?>/assets/videos/Green-Peak-Summer-Retreat-2018.mp4" type="video/mp4">
+                        </video>
+                    </div>
+                    <div class="col-12 video-block">
+                        <video width="100%" height="500px" controls>
+                            <source src="<?php echo get_template_directory_uri(); ?>/assets/videos/Green-Peak-Summer-Retreat-2018.mp4" type="video/mp4">
+                        </video>
+                    </div>
+                    <div class="col-12 video-block">
+                        <video width="100%" height="500px" controls>
+                            <source src="<?php echo get_template_directory_uri(); ?>/assets/videos/Green-Peak-Summer-Retreat-2018.mp4" type="video/mp4">
+                        </video>
+                    </div>
+                    <div class="col-12 video-block">
+                        <video width="100%" height="500px" controls>
+                            <source src="<?php echo get_template_directory_uri(); ?>/assets/videos/Green-Peak-Summer-Retreat-2018.mp4" type="video/mp4">
+                        </video>
+                    </div>
+                    <div class="col-12 video-block">
+                        <video width="100%" height="500px" controls>
+                            <source src="<?php echo get_template_directory_uri(); ?>/assets/videos/Green-Peak-Summer-Retreat-2018.mp4" type="video/mp4">
+                        </video>
+                    </div>
+                </div>
+            </div>
+        </section>
+    <!-- Video section ends here  -->
+
+    <!-- Article section starts here -->
+        <section class="article-wrapper" style="background-image: url('<?php echo get_template_directory_uri(); ?>/images/article-bg.png');">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 text-center">
+                        <h3 class="green-text">Green Peak in HBR: "How Private Equity Firms Hire CEOs"</h3>
+                        <p>J.P. Flaum and the Green Peak team partnered with Jeffrey Cohn to understand how leading PE 
+                            firms make their most critical hiring decisions – CEOs in their portfolio companies. The findings were shared in 
+                            the piece “How to Avoid Mistakes When Selecting the CEO: Lessons from Titans of Private Equity”...</p>
+                        
+                        <a href="read-article">read article</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="article-wrapper" style="background-image: url('<?php echo get_template_directory_uri(); ?>/images/article-bg.png');">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 text-center">
+                        <h3 class="blue-text">Green Peak in HBR: "How Private Equity Firms Hire CEOs"</h3>
+                        <p>J.P. Flaum and the Green Peak team partnered with Jeffrey Cohn to understand how leading PE 
+                            firms make their most critical hiring decisions – CEOs in their portfolio companies. The findings were shared in 
+                            the piece “How to Avoid Mistakes When Selecting the CEO: Lessons from Titans of Private Equity”...</p>
+                            
+                            <a href="read-article">read article</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="article-wrapper" style="background-image: url('<?php echo get_template_directory_uri(); ?>/images/article-bg.png');">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 text-center">
+                        <h3 class="green-text">Green Peak in HBR: "How Private Equity Firms Hire CEOs"</h3>
+                        <p>J.P. Flaum and the Green Peak team partnered   with Jeffrey Cohn to understand how leading PE 
+                            firms make their most critical hiring decisions – CEOs in their portfolio companies. The findings were shared in 
+                            the piece “How to Avoid Mistakes When Selecting the CEO: Lessons from Titans of Private Equity”...</p>
+
+                            <a href="read-article">read article</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+    <!-- Article section ends here -->
 
 <?php get_footer();
