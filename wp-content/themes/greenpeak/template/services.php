4c0e2ac533d12245d49107933d0c86f0ed39854e
vanessa.vishakha <vanessa.vishakha@ithands.biz>
2018-09-01 17:28:14 +0530

    Made services page dynamic

--- wp-content/themes/greenpeak/template/services.php
+++ wp-content/themes/greenpeak/template/services.php
@@ -3,105 +3,66 @@
 
 get_header(); ?>
 
-<div id="service-container">
-  <div id="service-container2">
-    <div class="box"  style="background-image: url('/wp-content/themes/greenpeak/images/ServicesMainBG_GP.jpg')">
-      <div>
-        <div class="col-main">
-          <div class="row">
-            <div class="col-6">
-                <p>Over nearly fifteen years, Green Peak has completed thousands of talent and organizational engagements for our demanding private equity and corporate clients. We build value across the deal cycle, offering diagnostic and execution support from diligence through exit. </p>
-            </div>
-            <div class="col-6 img-down">
-              <div class="content">
-                <h5>1.Over nearly fifteen years</h5>
-                <ul>
-                  <li>Evaluate fit of team, culture, and organization against your investment thesis </li>
-                  <li>Evaluate fit of team, culture, and organization against your investment thesis </li>
-                  <li>Evaluate fit of team, culture, and organization against your investment thesis </li>
-                </ul>
-              </div>
-              <img src="/wp-content/themes/greenpeak/images/DiligenceSupportGraphic_Services.png" alt="DiligenceSupportGraphic">
-            </div>
-            </div>
-        </div>
-
-        <div class="col-main">
-          <div class="row">
-            <div class="col-6 img-up">
-              <img src="/wp-content/themes/greenpeak/images/BuyRightGraphic_Services.png" alt="BuyRightGraphic_Service">
-              <div class="content">
-                <h5>2.Over nearly fifteen years</h5>
-                <ul>
-                  <li>Evaluate fit of team, culture, and organization against your investment thesis </li>
-                  <li>Evaluate fit of team, culture, and organization against your investment thesis </li>
-                  <li>Evaluate fit of team, culture, and organization against your investment thesis </li>
-                </ul>
-              </div>
-            </div>
-            <div class="col-6 img-down">
-              <div class="content">
-                    <h5>3.Over nearly fifteen years</h5>
-                    <ul>
-                      <li>Evaluate fit of team, culture, and organization against your investment thesis </li>
-                      <li>Evaluate fit of team, culture, and organization against your investment thesis </li>
-                      <li>Evaluate fit of team, culture, and organization against your investment thesis </li>
-                    </ul>
-            </div>
-                <img src="/wp-content/themes/greenpeak/images/First100DaysGraphic_Services.png" alt="First100DaysGraphic_Services">
-            </div>
-            </div>
-        </div>
-
-        <div class="col-main">
-          <div class="row">
-            <div class="col-6 img-up">
-              <img src="/wp-content/themes/greenpeak/images/MaxValueGraphic_Services.png" alt="MaxValueGraphic_Services">
-                  <div class="content">
-              <h5>2.Over nearly fifteen years</h5>
-              <ul>
-                <li>Evaluate fit of team, culture, and organization against your investment thesis </li>
-                <li>Evaluate fit of team, culture, and organization against your investment thesis </li>
-                <li>Evaluate fit of team, culture, and organization against your investment thesis </li>
-              </ul>
-              </div>
-            </div>
-            <div class="col-6 img-down">
-            <div class="content">
-                  <h5>3.Over nearly fifteen years</h5>
-                  <ul>
-                    <li>Evaluate fit of team, culture, and organization against your investment thesis </li>
-                    <li>Evaluate fit of team, culture, and organization against your investment thesis </li>
-                    <li>Evaluate fit of team, culture, and organization against your investment thesis </li>
-                  </ul>
-              </div>
-                <img src="/wp-content/themes/greenpeak/images/HighValueExitGraphic_Services.png" alt="HighValueExitGraphic_Services">
-            </div>
+<?php $services = get_field('services');
+$count = 1;
+$flag = 0;
+if (!empty($services)): ?>
+    <div id="service-container">
+        <div id="service-container2">
+            <div class="box" style="background-image: url('<?php the_field('background_image'); ?>')">
+                <div>
+                    <div class="col-main">
+                        <div class="col-6 para-div">
+                            <p><?php the_field('content',false,false); ?></p>
+                        </div>
+                        <div class="row">
+                            <?php foreach ($services as $service):
+                            if ($count % 2 != 0) {
+                                ?>
+                                <div class="col-6 img-up">
+                                    <?php if ($service['image']) { ?>
+                                        <img src="<?php echo $service['image']; ?>"
+                                             alt="DiligenceSupportGraphic">
+                                    <?php } ?>
+                                    <div class="content">
+                                        <?php if ($service['main_heading']) { ?>
+                                            <h5><?php echo $service['main_heading']; ?></h5>
+                                        <?php } ?>
+                                        <?php echo $service['description']; ?>
+                                    </div>
+                                </div>
+                            <?php } else {
+                                ?>
+                                <div class="col-6 img-down">
+                                    <div class="content">
+                                        <?php if ($service['main_heading']) { ?>
+                                            <h5><?php echo $service['main_heading']; ?></h5>
+                                        <?php } ?>
+                                        <?php echo $service['description']; ?>
+                                    </div>
+                                    <?php if ($service['image']) { ?>
+                                        <img src="<?php echo $service['image']; ?>"
+                                             alt="DiligenceSupportGraphic">
+                                    <?php } ?>
+                                </div>
+                                <?php
+                            } ?>
+                            <?php
+                            if ($count % 2 == 0){
+                            ?>
+                        </div>
+                    </div>
+                    <div class="col-main">
+                        <div class="row">
+                            <?php }
+                            $count++;
+                            endforeach; ?>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-
-
-        <div class="col-main">
-          <div class="row">
-            <div class="col-sm-12 img-up">
-              <img src="/wp-content/themes/greenpeak/images/SellHighGraphic_Services.png" alt="SellHighGraphic_Services">
-                    <div class="content">
-              <h5>2.Over nearly fifteen years</h5>
-              <ul>
-                <li>Evaluate fit of team, culture, and organization against your investment thesis </li>
-                <li>Evaluate fit of team, culture, and organization against your investment thesis </li>
-                <li>Evaluate fit of team, culture, and organization against your investment thesis </li>
-              </ul>
-              </div>
-            </div>
-            </div>
-        </div>
-
-      </div>
     </div>
-
-  </div>
-</div>
-
+<?php endif; ?>
 
 <?php get_footer();
