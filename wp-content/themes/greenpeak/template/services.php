513fca41d19c1c70a6a06353bfac5457c17edfa3
erica urvashi <erica.urvashi@ithands.biz>
2018-09-01 14:43:56 +0530

    service page set

--- wp-content/themes/greenpeak/template/services.php
+++ wp-content/themes/greenpeak/template/services.php
@@ -4,17 +4,18 @@
 get_header(); ?>
 
 <div id="service-container">
-  <svg id="mySVG">
-  <path fill="none" stroke="red" stroke-width="3" id="triangle" d="M0,0.5h4035" />
-  Sorry, your browser does not support inline SVG.
-  </svg>
   <div id="service-container2">
     <div class="box"  style="background-image: url('/wp-content/themes/greenpeak/images/ServicesMainBG_GP.jpg')">
       <div>
         <div class="col-main">
           <div class="row">
-            <div class="col-6">
+            <div class="col-6 para-div">
                 <p>Over nearly fifteen years, Green Peak has completed thousands of talent and organizational engagements for our demanding private equity and corporate clients. We build value across the deal cycle, offering diagnostic and execution support from diligence through exit. </p>
+                <svg id="more-arrows">
+                  <polygon class="arrow-top" points="37.6,27.9 1.8,1.3 3.3,0 37.6,25.3 71.9,0 73.7,1.3 "/>
+                  <polygon class="arrow-middle" points="37.6,45.8 0.8,18.7 4.4,16.4 37.6,41.2 71.2,16.4 74.5,18.7 "/>
+                  <polygon class="arrow-bottom" points="37.6,64 0,36.1 5.1,32.8 37.6,56.8 70.4,32.8 75.5,36.1 "/>
+                </svg>
             </div>
             <div class="col-6 img-down">
               <div class="content">
