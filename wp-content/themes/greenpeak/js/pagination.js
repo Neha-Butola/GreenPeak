a58a85763e170891f6a164356187d594a2c8f52e
vanessa.vishakha <vanessa.vishakha@ithands.biz>
2018-09-04 11:13:11 +0530

    Add js for articles sharing icons

--- wp-content/themes/greenpeak/js/pagination.js
+++ wp-content/themes/greenpeak/js/pagination.js
@@ -4,7 +4,7 @@ jQuery(document).ready(function ($) {
         var page = $(this).attr('page');
         var total_pages = $(this).attr('total-pages');
         $('.video-load-more').attr('page', parseInt(page) + 1);
-        if(page <= total_pages){
+        if (page <= total_pages) {
             $.ajax({
                 url: ajaxpagination.ajaxurl,
                 type: 'post',
@@ -19,9 +19,20 @@ jQuery(document).ready(function ($) {
                 }
             })
         }
-        if(total_pages === page){
+        if (total_pages === page) {
             $('.nav-links').hide(0);
         }
     });
+    /**
+     * fires when social sharing icons are clicked
+     * this function opens a popup to share content to social media
+     */
+    $('a.resp-sharing-button__link').on('click', function (e) {
+        e.preventDefault();
+        var width = $(window).width() / 2;
+        var height = $(window).height() / 2;
+        var left = (screen.width / 2) - (width / 2);
+        var top = (screen.height / 2) - (height / 2);
+        window.open(this.href, '', ' scrollbars=yes,menubar=no,width=' + width + ',height=' + height + ',resizable=yes,toolbar=no,location=no,status=no,top=' + top + ', left=' + left);
+    });
 });
-
