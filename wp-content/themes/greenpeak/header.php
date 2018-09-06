dcc23480815225a728734d74b3abbfaf407e08ae
vanessa.vishakha <vanessa.vishakha@ithands.biz>
2018-09-06 17:47:36 +0530

    Made navigation menu dynamic

--- wp-content/themes/greenpeak/header.php
+++ wp-content/themes/greenpeak/header.php
@@ -13,46 +13,33 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="https://gmpg.org/xfn/11">
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="https://gmpg.org/xfn/11">
 
-	<?php wp_head(); ?>
+    <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'greenpeak' ); ?></a>
+<div class="app">
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$greenpeak_description = get_bloginfo( 'description', 'display' );
-			if ( $greenpeak_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $greenpeak_description; /* WPCS: xss ok. */ ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
+    <main>
+        <!-- * =============== Header =============== * -->
+        <header class="main-head fixed-top">
+            <nav class="container">
+                <a href="/" class="logo"><img src="<?php the_field('header_logo','options'); ?>"
+                                              alt="greenpeak-logo"></a>
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'greenpeak' ); ?></button>
-			<?php
-			wp_nav_menu( array(
-				'theme_location' => 'menu-1',
-				'menu_id'        => 'primary-menu',
-			) );
-			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+                <?php wp_nav_menu([
+                    'container' => '',
+                    'menu_class' => 'list-unstyled mb-0',
+                ]); ?>
 
-	<div id="content" class="site-content">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample04"
+                        aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+            </nav>
+        </header>
