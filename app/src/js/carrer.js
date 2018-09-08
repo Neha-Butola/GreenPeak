1d69540987f6a919213dcd476102370784180327
Nancy.Shalini <nancy.shalini@ithands.biz>
2018-09-08 11:30:06 +0530

    add career page

--- app/src/js/carrer.js
+++ app/src/js/carrer.js
@@ -1,16 +1,34 @@
-$('.owl-carousel').owlCarousel({
+$('.testimonial .owl-carousel').owlCarousel({
   loop: true,
   margin: 10,
-  nav: true,
+  dots: true,
+  smartSpeed: 600,
   responsive: {
     0: {
       items: 1
     },
     600: {
-      items: 3
+      items: 1
     },
     1000: {
       items: 1
     }
   }
+});
+
+$('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+  disableOn: 700,
+  type: 'iframe',
+  mainClass: 'mfp-fade',
+  removalDelay: 160,
+  preloader: false,
+
+});
+
+$('.thevideo').mouseover(function () {
+  $(this).get(0).play();
+  $('video').addClass('play-video');
+}).mouseout(function () {
+  $(this).get(0).pause();
+  $('video').removeClass('play-video');
 })
