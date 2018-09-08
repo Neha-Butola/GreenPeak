7d24127adff4d45773c734c513c3cf199f772d22
Max Gaurav <max.gaurav@ithands.net>
2018-09-08 14:55:53 +0530

    add: animation for horizontal timeline

--- app/dist/js/app.js
+++ app/dist/js/app.js
@@ -318,6 +318,31 @@ owl.on('mousewheel', '.owl-stage', function (e) {
     }
     e.preventDefault();
 });
+
+$('.service-container').on('scroll', () => {
+    let elements = $('.service-col');
+    let offset = $('.service-container').scrollTop();
+
+    let indentLeft = 90;
+    elements.each((index, item) => {
+        if (index === 0) {
+            return;
+        }
+
+        let ele = $(item);
+        // debugger;
+        if (ele.offset().left + (500 + indentLeft) <= offset) {
+            indentLeft = indentLeft + 90;
+            console.log(offset, ele.offset().left);
+            // percentage = offset / (ele.offset().left + 500);
+            // percentage = percentage >= 100 ? 100 : percentage;
+            // console.log(percentage);
+            ele.find('.timeline-inner').css('width', '108%');
+        } else {
+            ele.find('.timeline-inner').css('width', '0');
+        }
+    });
+});
 /* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! jquery */ "jquery")))
 
 /***/ }),
--- app/src/js/service.js
+++ app/src/js/service.js
@@ -23,3 +23,49 @@ owl.on('mousewheel', '.owl-stage', function (e) {
     }
     e.preventDefault();
 });
+
+$('.service-container').on('scroll', () => {
+    let elements = $('.service-col');
+    let offset = $('.service-container').scrollTop();
+
+    let indentLeft= 90;
+    elements.each((index, item) => {
+        if (index === 0) {
+            return;
+        }
+
+        let ele = $(item);
+        // debugger;
+        if (ele.offset().left + (500 + indentLeft) <= offset) {
+            indentLeft = indentLeft + 90;
+            console.log(offset, ele.offset().left);
+            // percentage = offset / (ele.offset().left + 500);
+            // percentage = percentage >= 100 ? 100 : percentage;
+            // console.log(percentage);
+            if (index <= (elements.length - 2)) {
+                ele.find('.timeline-inner').css('width', '108%');
+            } else {
+                ele.find('.timeline-inner').css('width', '5%');
+            }
+
+
+            let dotEle = ele.find('.dot div');
+            ele.find('.dot').css('background', '#fff');
+            dotEle.css('height', '80px');
+            if (index % 2 === 0) {
+                dotEle.css('top', '-80px');
+            }
+        } else {
+            ele.find('.timeline-inner').css('width', '0');
+            ele.find('.dot').css('background', 'rgba(0,0,0,0)');
+
+            let dotEle = ele.find('.dot div');
+            dotEle.css('height', '0');
+            if (index % 2 === 0) {
+                dotEle.css('top', '0');
+            }
+        }
+    });
+
+
+});
