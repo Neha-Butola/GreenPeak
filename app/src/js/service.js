07016329df07f9680e2c6b2ea4d7edb5cf924c62
Max Gaurav <max.gaurav@ithands.net>
2018-09-08 15:28:39 +0530

    add: vertical line block animation

--- app/src/js/service.js
+++ app/src/js/service.js
@@ -13,20 +13,17 @@ $('.service-container').on('scroll', () => {
         // debugger;
         if (ele.offset().left + (500 + indentLeft) <= offset) {
             indentLeft = indentLeft + 90;
-            console.log(offset, ele.offset().left);
-            // percentage = offset / (ele.offset().left + 500);
-            // percentage = percentage >= 100 ? 100 : percentage;
-            // console.log(percentage);
+
             if (index <= (elements.length - 2)) {
                 ele.find('.timeline-inner').css('width', '108%');
             } else {
                 ele.find('.timeline-inner').css('width', '5%');
             }
 
-
             let dotEle = ele.find('.dot>div');
             ele.find('.dot').addClass('show');
             dotEle.css('height', '80px');
+            dotEle.find('.dot-in').css("display",'block');
             if (index % 2 === 0) {
                 dotEle.css('top', '-80px');
             }
@@ -36,6 +33,7 @@ $('.service-container').on('scroll', () => {
 
             let dotEle = ele.find('.dot>div');
             dotEle.css('height', '0');
+            dotEle.find('.dot-in').css("display",'none');
             if (index % 2 === 0) {
                 dotEle.css('top', '0');
             }
