8f0e79410435b57366eb92b55bad8a9de8b8c1e1
vanessa.vishakha <vanessa.vishakha@ithands.biz>
2018-09-08 18:57:51 +0530

    Enqueued css and js files

--- wp-content/themes/greenpeak/functions.php
+++ wp-content/themes/greenpeak/functions.php
@@ -168,3 +168,28 @@ if (function_exists('acf_add_options_page')) {
     ));
 
 }
+
+/*
+ * Videos custom post type function
+ */
+function create_posttype()
+{
+
+    register_post_type('videos',
+        array(
+            'labels' => array(
+                'name' => __('Videos'),
+                'singular_name' => __('Video')
+            ),
+            'public' => true,
+            'has_archive' => true,
+            'rewrite' => array('slug' => 'videos'),
+        )
+    );
+}
+
+/*
+ * Hooking up our function to theme setup
+ */
+add_action('init', 'create_posttype');
+
