a4069c0735d66e13e556f1e7c70a37b887305d56
vanessa.vishakha <vanessa.vishakha@ithands.biz>
2018-09-09 18:33:45 +0530

    Updated the structure for services page

--- wp-content/themes/greenpeak/templates/services.php
+++ wp-content/themes/greenpeak/templates/services.php
@@ -4,31 +4,34 @@
  */
 get_header(); ?>
 
-    <img src="<?php the_field('background_image'); ?>" alt="" class="bg-image">
-    <div class="service-container">
+    <img src="<?php the_field('background_image'); ?>" alt="services-background-image" class="bg-image">
+    <div class="service-container dragscroll">
         <div class="service-container2">
-            <div class="service-page">
+            <div class="service-section" id="mydiv">
                 <div class="service-wrap">
-                    <div class="service-col timeline_active">
-                        <div class="content-below">
-                            <h4 class="">
-                                <?php the_field('content'); ?>
-                            </h4>
-                        </div><!--/service-col-->
+                    <div class="service-col">
+                        <h4>
+                            <?php the_field('content'); ?>
+                        </h4>
+                        <svg class="arrows_svg">
+                            <polygon class="arrow-top" points="37.6,27.9 1.8,1.3 3.3,0 37.6,25.3 71.9,0 73.7,1.3 "></polygon>
+                            <polygon class="arrow-middle" points="37.6,45.8 0.8,18.7 4.4,16.4 37.6,41.2 71.2,16.4 74.5,18.7 "></polygon>
+                            <polygon class="arrow-bottom" points="37.6,64 0,36.1 5.1,32.8 37.6,56.8 70.4,32.8 75.5,36.1 "></polygon>
+                        </svg>
                     </div>
                     <?php $services = get_field('services');
                     if (!empty($services)):
                         $count = 0;
                         foreach ($services as $service):
                             if ($count % 2 == 0):?>
-                                <div class="service-col content-below timeline_active">
-                                    <div class="timeline-inner">
+                                <div class="service-col">
+                                    <div class="timeline">
                                         <div class="dot">
                                             <div>
                                                 <div class="dot-in"></div>
                                             </div>
                                         </div>
-                                    </div> <!--/timeline-inner-->
+                                    </div> <!--/timeline-->
                                     <div class="col-above">
                                         <h4 class="title"><?php echo $service['main_heading']; ?></h4>
                                         <?php echo $service['description']; ?>
@@ -38,14 +41,14 @@ get_header(); ?>
                                     </div>
                                 </div><!--/service-col-->
                             <?php else: ?>
-                                <div class="service-col timeline_active">
-                                    <div class="timeline-inner">
+                                <div class="service-col">
+                                    <div class="timeline">
                                         <div class="dot">
                                             <div>
                                                 <div class="dot-in"></div>
                                             </div>
                                         </div>
-                                    </div> <!--/timeline-inner-->
+                                    </div> <!--/timeline-->
                                     <div class="col-above">
                                         <img src="<?php echo $service['image']; ?>" alt="" class="">
                                     </div><!--/col-above-->
