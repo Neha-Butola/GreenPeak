8b1a19e8877b0eceb67bd1dc962555c00f5c5190
natalieneha <natalie.neha@ithands.biz>
2018-09-09 15:22:42 +0530

    update service page

--- app/src/js/service.js
+++ app/src/js/service.js
@@ -1,9 +1,94 @@
 
+// // var dargelem = $('.service-wrap');
+// dragElement(document.getElementById("mydiv"));
+
+// function dragElement(elmnt) {
+//     var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+   
+//       /* otherwise, move the DIV from anywhere inside the DIV:*/
+//       elmnt.onmousedown = dragMouseDown;
+    
+  
+//     function dragMouseDown(e) {
+//       e = e || window.event;
+//       e.preventDefault();
+//       // get the mouse cursor position at startup:
+//       pos3 = e.clientX;
+//       pos4 = e.clientY;
+//       document.onmouseup = closeDragElement;
+//       // call a function whenever the cursor moves:
+//       document.onmousemove = elementDrag;
+//     }
+  
+//     function elementDrag(e) {
+//       e = e || window.event;
+//       e.preventDefault();
+//       // calculate the new cursor position:
+//       pos1 = pos3 - e.clientX;
+//       pos2 = pos4 - e.clientY;
+//       pos3 = e.clientX;
+//       pos4 = e.clientY;
+//       // set the element's new position:
+//       elmnt.style.left = ((elmnt.offsetLeft - pos1 )) + "px";
+//     }
+  
+//     function closeDragElement() {
+//       /* stop moving when mouse button is released:*/
+//       document.onmouseup = null;
+//       document.onmousemove = null;
+//     }
+//   }
+
+// var clicked = false, clickY;
+
+// $('.service-container').on({
+//     'mousemove': function(e) {
+//         clicked && updateScrollPos(e);
+//     },
+//     'mousedown': function(e) {
+//         clicked = true;
+//         clickY = e.pageY;
+//         console.log($('.service-container').scrollTop());
+//     },
+//     'mouseup': function() {
+//         clicked = false;
+//         // $('.service-page').css('cursor', 'auto');
+//     }
+// });
+
+// var updateScrollPos = function(e) {
+//     // $('.service-page').css('cursor', 'row-resize');
+//     $('.service-container').scrollTop($('.service-container').scrollTop() + (clickY - e.pageY));
+// }
+
+
+// $(function(){
+//     var curDown = false,
+//         curYPos = 0,
+//         curXPos = 0;
+//     $(window).mousemove(function(m){
+//       if(curDown === false){
+//        $('.service-container').scrollTop($(window).scrollTop() + (curXPos - m.pageX)); 
+//     //    $('.service-container').scrollLeft($(window).scrollLeft() + (curXPos - m.pageX));
+//       }
+//     });
+    
+//     $(window).mousedown(function(m){
+//       curDown = true;
+//       curYPos = m.pageY;
+//       curXPos = m.pageX;
+//     });
+    
+//     $(window).mouseup(function(){
+//       curDown = false;
+//     });
+//   })
+
 $('.service-container').on('scroll', () => {
     let elements = $('.service-col');
-    let offset = $('.service-container').scrollTop();
+    let offset = $('.service-container').scrollTop() + 600;
 
-    let indentLeft= 90;
+    let indentLeft= -90;
     elements.each((index, item) => {
         if (index === 0) {
             return;
@@ -18,9 +103,9 @@ $('.service-container').on('scroll', () => {
             indentLeft = indentLeft + 90;
 
             if (index <= (elements.length - 2)) {
-                ele.find('.timeline-inner').css('width', '108%');
+                ele.find('.timeline').css('width', '111%');
             } else {
-                ele.find('.timeline-inner').css('width', '5%');
+                ele.find('.timeline').css('width', '0');
             }
 
             let dotEle = ele.find('.dot>div');
@@ -35,7 +120,7 @@ $('.service-container').on('scroll', () => {
                 $('header').removeClass('nav-style');
             }
 
-            ele.find('.timeline-inner').css('width', '0');
+            ele.find('.timeline').css('width', '0');
             ele.find('.dot').removeClass('show');
 
             let dotEle = ele.find('.dot>div');
@@ -49,3 +134,4 @@ $('.service-container').on('scroll', () => {
 
 
 });
+
