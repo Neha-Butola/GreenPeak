f8219b9729392a7cf72026d547171ce460e573bd
vanessa.vishakha <vanessa.vishakha@ithands.biz>
2018-09-12 17:48:43 +0530

    Updated the video section on banner

--- wp-content/themes/greenpeak/templates/homepage.php
+++ wp-content/themes/greenpeak/templates/homepage.php
@@ -13,12 +13,12 @@ get_header(); ?>
             <div class="animating-line"></div>
             <h1 data-aos="fade-in" data-aos-delay="400"><span><?php the_field('banner_sub_heading'); ?></span></h1>
         </div>
-        <!--        <video class="thevideo w-100 " autoplay="autoplay" loop="loop" playsinline preload="none" muted=""-->
-        <!--               poster="--><?php //the_field('banner_image'); ?><!--">-->
-        <!--            <source src="--><?php //the_field('banner_video'); ?><!--"-->
-        <!--                    type="video/mp4">-->
-        <!--            Your browser does not support the video tag.-->
-        <!--        </video>-->
+        <video id="banner-video" autoplay="autoplay" loop="loop" playsinline preload="none" muted=""
+               poster="<?php the_field('banner_image'); ?>">
+            <source src="<?php the_field('banner_video'); ?>"
+                    type="video/mp4">
+            Your browser does not support the video tag.
+        </video>
 
     </div>
     <!-- * =============== /Banner section =============== * -->
@@ -46,7 +46,8 @@ if (!empty($content)): ?>
                         <div class="col-md-6 <?php echo ($count % 2 != 0) ? 'text-md-right' : 'offset-md-6'; ?>"
                              data-aos="fade-in" data-aos-delay="100">
                             <h2>
-                                <a href="<?php echo $section['main_heading_link']; ?>">“<?php echo $section['main_heading']; ?>”</a>
+                                <a href="<?php echo $section['main_heading_link']; ?>">“<?php echo $section['main_heading']; ?>
+                                    ”</a>
                             </h2>
                             <h3><?php echo $section['sub_heading']; ?></h3>
                         </div>
