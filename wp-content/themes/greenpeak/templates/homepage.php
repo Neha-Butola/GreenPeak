8c33eb3885849d687882fddaa99d3f2adee868ed
vanessa.vishakha <vanessa.vishakha@ithands.biz>
2018-09-14 17:29:35 +0530

    Add class to triangle section

--- wp-content/themes/greenpeak/templates/homepage.php
+++ wp-content/themes/greenpeak/templates/homepage.php
@@ -27,7 +27,7 @@ get_header(); ?>
     <!-- * =============== triangle section =============== * -->
     <canvas id="animating-triangle" style="background-image:url('<?php the_field('background_image'); ?>');"
             class="d-none d-md-block fixed-bg"></canvas>
-    <img src="<?php the_field('background_image'); ?>" alt="" class="img-fluid d-md-none partners-image">
+    <img src="<?php the_field('background_image'); ?>" alt="" class="img-fluid d-block d-md-none">
     <!-- * =============== /triangle section =============== * -->
 
 <?php $content = get_field('content');
