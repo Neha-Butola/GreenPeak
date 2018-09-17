49cbc1bd26d02ef4feedbfe457c7e66ea6f304d3
natalieneha <natalie.neha@ithands.biz>
2018-09-17 16:56:27 +0530

    ui updates

--- wp-content/themes/greenpeak/dist/js/app.js
+++ wp-content/themes/greenpeak/dist/js/app.js
@@ -134,19 +134,17 @@ $('.thevideo').magnificPopup({
 
 // Header scroll
 
-$(window).resize(function () {
-  var titlewidth;
-  titlewidth = $('.banner-content h1 span').width();
-  valuetitle = $('.values-title h2 span').width();
-  titlewidth = titlewidth / 2;
-  valuetitle = valuetitle / 2;
-  $('.banner-content .animating-line').css({
-    'width': 'calc(50% + ' + titlewidth + 'px)'
-  });
-  $('.value-sec .animating-line').css({
-    'width': 'calc(50% + ' + valuetitle + 'px)'
-  });
-});resize();
+var titlewidth;
+titlewidth = $('.banner-content h1 span').width();
+valuetitle = $('.values-title h2 span').width();
+titlewidth = titlewidth / 2;
+valuetitle = valuetitle / 2;
+$('.banner-content .animating-line').css({
+  'width': 'calc(50% + ' + titlewidth + 'px)'
+});
+$('.value-sec .animating-line').css({
+  'width': 'calc(50% + ' + valuetitle + 'px)'
+});
 
 $(window).scroll(function () {
   var scroll = $(window).scrollTop();
@@ -180,22 +178,31 @@ $('#banner-video').each(function () {
 });
 
 window.onload = function () {
-  $('.site-brand').addClass('show-fade').one('webkitAnimationEnd mozAnimationEnd oAnimationEnd animationEnd', function () {
+  if ($('.banner').hasClass('home-banner')) {
+    $('.site-brand').addClass('show-fade').one('webkitAnimationEnd mozAnimationEnd oAnimationEnd animationEnd', function () {
+      $('.banner-content h1').addClass('show-fade').one('webkitAnimationEnd mozAnimationEnd oAnimationEnd animationEnd', function () {
+        $('.banner-content .animating-line').addClass('show-line');
+      });
+    });
+  } else {
     $('.banner-content h1').addClass('show-fade').one('webkitAnimationEnd mozAnimationEnd oAnimationEnd animationEnd', function () {
       $('.banner-content .animating-line').addClass('show-line');
+      $('.banner-content .right-line').addClass('show-right_line');
     });
-  });
+  }
 
-  var vid = document.getElementById("banner-video");
+  if (document.querySelector('#banner-video')) {
+    var vid = document.getElementById("banner-video");
 
-  setTimeout(function () {
-    vid.play(); //will play after 10 seconds when video is ended
-  }, 100);
-
-  if ($('.banner').hasClass('video-banner')) {
     setTimeout(function () {
-      $('.banner-content h1').addClass('show-fadeOut');
-    }, 5000);
+      vid.play(); //will play after 10 seconds when video is ended
+    }, 100);
+
+    if ($('.banner').hasClass('video-banner')) {
+      setTimeout(function () {
+        $('.banner-content h1').addClass('show-fadeOut');
+      }, 5000);
+    }
   }
 };
 /* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! jquery */ "jquery")))
